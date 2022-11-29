36d56eb4fefc3b85e20eaa8ef7a6865f7386b2e3
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2022-11-29 13:04:27 +0100

    add structure, navigation and links

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Image from "next/image";
 import {useRouter} from "next/router";
 import StyledLink from "./StyledLink";
-import Metamask from "../public/images/Metamask.svg";
+import Metamask from "../public/images/metamask.svg";
 import Funds from "../public/images/funds.svg";
 import Home from "../public/images/home.svg";
 import Profile from "../public/images/profile.svg";
