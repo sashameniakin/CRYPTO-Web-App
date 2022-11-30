3fe3702a22edc7923e2aedc7b0351b6f22dae207
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2022-11-30 20:49:23 +0100

    fixed some issues

--- pages/_app.js
+++ pages/_app.js
@@ -4,28 +4,19 @@ import GlobalStyles from "../styles/GlobalStyles";
 import {useRouter} from "next/router";
 
 function MyApp({Component, pageProps}) {
-  const router = useRouter();
-  const {asPath} = router;
-  console.log(asPath);
+  const {asPath} = useRouter();
 
-  if (asPath === "/login" || asPath === "/") {
-    return (
-      <>
-        <GlobalStyles />
-
+  return (
+    <>
+      <GlobalStyles />
+      {asPath === "/login" || asPath === "/" ? (
         <Component {...pageProps} />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <GlobalStyles />
-
+      ) : (
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </>
-    );
-  }
+      )}
+    </>
+  );
 }
 export default MyApp;
