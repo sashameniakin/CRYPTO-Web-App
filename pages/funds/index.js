7a47b9ab582d5a9bc92c71adfc420eb48a956575
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2022-12-01 12:09:39 +0100

    fix lottie 7

--- pages/funds/index.js
+++ pages/funds/index.js
@@ -1,10 +1,3 @@
-import LottieComponent from "..";
-
 export default function Funds() {
-  return (
-    <p>
-      Page Funds is under construction
-      <LottieComponent />
-    </p>
-  );
+  return <p>Page Funds is under construction</p>;
 }
