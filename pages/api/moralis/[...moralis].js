017eef851259c0312ab9fe42b2bf8a869751c79a
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2022-12-13 10:32:21 +0100

    fix bugs 5

--- pages/api/moralis/[...moralis].js
+++ pages/api/moralis/[...moralis].js
@@ -1,10 +1,10 @@
 import {MoralisNextApi} from "@moralisweb3/next";
 
 export default MoralisNextApi({
-  apiKey: process.env.MORALIS_API_KEY,
+  /*   apiKey: process.env.MORALIS_API_KEY,
   authentication: {
     domain: "amazing.dapp",
     uri: process.env.NEXTAUTH_URL,
     timeout: 120,
-  },
+  }, */
 });
