f6873a5b5c8841a7b0c2130b80716c0d4c663e4d
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2022-12-14 15:07:49 +0100

    add structure of task card

--- components/Task.js
+++ components/Task.js
@@ -1,15 +1,40 @@
 import styled from "styled-components";
 import {StyledButton} from "./Popup_bookmarked";
+import {StyledImage} from "./Header";
+import Archive from "../public/images/archive.svg";
+import Delete from "../public/images/delete.svg";
+import Details from "../public/images/details.svg";
+import {useGlobalState} from "../state";
 
 export default function Task() {
+  const [] = useGlobalState();
+  function handleDelete(id) {}
+  function handleDetails(id) {}
+
   return (
     <>
       <StyledContainer>
-        <StyledButton></StyledButton>
+        <StyledButton>
+          <StyledImage alt="archive" src={Archive} width="50px" height="50px" />
+        </StyledButton>
         <Titel></Titel>
         <StyledFlexContainer>
-          <StyledButton></StyledButton>
-          <StyledButton></StyledButton>
+          <StyledButton onClick={() => handleDelete(id)}>
+            <StyledImage
+              alt="archive"
+              src={Delete}
+              width="25px"
+              height="25px"
+            />
+          </StyledButton>
+          <StyledButton onClick={() => handleDetails(id)}>
+            <StyledImage
+              alt="details"
+              src={Details}
+              width="25px"
+              height="25px"
+            />
+          </StyledButton>
         </StyledFlexContainer>
       </StyledContainer>
     </>
@@ -23,4 +48,8 @@ const StyledContainer = styled.div`
   align-items: center;
 `;
 const Titel = styled.p``;
-const StyledFlexContainer = styled.div``;
+const StyledFlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+`;
